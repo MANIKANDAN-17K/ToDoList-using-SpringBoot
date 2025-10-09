103546d2c042a12669ccf8871974fb2a9175c4d4
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-10-10 04:06:00 +0530

    Update TodoSource.java

--- src/main/java/com/example/helloDeveloperMani/TodoSource.java
+++ src/main/java/com/example/helloDeveloperMani/TodoSource.java
@@ -2,7 +2,12 @@ package com.example.helloDeveloperMani;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.example.helloDeveloperMani.models.Todo;
@@ -18,6 +23,12 @@ public class TodoSource {
 	public Todo getTodoById(Long id) {
 		return todoRepositary.findById(id).orElseThrow(() -> new RuntimeException("todo not found"));
 	}
+	
+	public Page<Todo> getAllTodosPages(int page,int size){
+		Pageable pageable = PageRequest.of(page, size);
+		return todoRepositary.findAll(pageable);
+	}
+	
 	public List<Todo> getToDos(){
 		return todoRepositary.findAll();
 	}
